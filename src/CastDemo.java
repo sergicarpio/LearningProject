fc0718e8822cc6a80638fc645b5e896fb5b05695
@sergicarpio <sergi.carpio.ortega@gmail.com>
2021-07-27 15:51:53 +0200

    cast demo class

---
+++ src/CastDemo.java
@@ -0,0 +1,28 @@
+public class CastDemo {
+    public static void main(String[] args) {
+        double x,y;
+        byte b;
+        int i;
+        char ch;
+
+        x= 10.0;
+        y = 3.0;
+
+        i = (int) (x/y);
+        System.out.println("Integer outcome x/y: " +i);
+
+        i=100;
+        b= (byte) i;
+        System.out.println("Value b: " +b);
+
+        i=257;
+        b= (byte) i;
+        System.out.println("Value b: " + b);
+
+        b=88; //Codi ASCII per a X
+        ch = (char) b;
+        System.out.println("ch:" +ch);
+
+    }
+
+}
